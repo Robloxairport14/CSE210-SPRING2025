74e7ce2ba4918f1c642abc4967864e284ab583eb
Dylan Scott Durtschi <dur21020@byui.edu>
2025-06-24 17:53:20 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int num = 1;
+        while (num != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            num = int.Parse(Console.ReadLine());
+            numbers.Add(num);
+        }
+        int big = numbers[0];
+        int Total = 0;
+        foreach (int number in numbers)
+        {
+            if (number != 0)
+            {
+                Total += number;
+            }
+            if (number > big)
+            {
+                big = number;
+            }
+        }
+        Console.WriteLine("The total is: " + Total);
+        Console.WriteLine("The average is: " + (Total / (numbers.Count)));
+        Console.WriteLine($"The largest number is:{big}");
     }
 }
