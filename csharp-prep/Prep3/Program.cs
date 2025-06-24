e0ce37f7fcc97b12df2f0926b07cbd7a42cf4db6
Dylan Scott Durtschi <dur21020@byui.edu>
2025-06-24 17:22:46 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int random = new Random().Next(1, 100);
+        int attempts = 1;
+        int guess = 0;
+        while (guess != random)
+        {
+            Console.WriteLine("Guess a number between 1 and 100:");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < random)
+            {
+                Console.WriteLine("Too low! Try again:");
+            }
+            else if (guess > random)
+            {
+                Console.WriteLine("Too high! Try again:");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You've guessed the number!");
+                Console.WriteLine($"It took you {attempts} attempts.");
+            }
+            attempts++;
+        }
     }
 }
