34e1a448b7f47efb4fe28a4fcfcdf61e5de9688c
Dylan Scott Durtschi <dur21020@byui.edu>
2025-06-24 18:18:44 -0600

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,38 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("favorite number:");
+        int num = int.Parse(Console.ReadLine());;
+        return num;
+    }
+    static int SquareNumber(int num)
+    {
+        int square = num * num;
+        return square;
+    }
+    static void DisplayResult(int square, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int square = SquareNumber(num);
+        DisplayResult(square, name);
+
     }
 }
